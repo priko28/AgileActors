b2a4a852869de1bd38cd7e1317c6a0b39a1bb13e
Kosmas Priftis <kosmaspriftis@hotmail.com>
2024-09-29 20:14:11 +0300

    cache service

---
+++ ApiAggregationService/Services/CacheService.cs
@@ -0,0 +1,27 @@
+﻿using AggregationService.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AggregationService.Services
+{
+    public class CacheService(IMemoryCache cache) : ICacheService
+    {
+        private readonly IMemoryCache _cache = cache;
+
+        // Get method to retrieve data from cache
+        public T? Get<T>(string cacheKey)
+        {
+            return _cache.TryGetValue(cacheKey, out T cachedData) ? cachedData : default;
+        }
+
+        // Set method to store data in cache
+        public void Set<T>(string cacheKey, T data, TimeSpan expiration)
+        {
+            var cacheOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expiration
+            };
+
+            _cache.Set(cacheKey, data, cacheOptions);
+        }
+    }
+}
